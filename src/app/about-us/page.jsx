c22dbf940262af29a74d189a7bca25cb9d4009ed
samik singh <samiksingh123@gmail.com>
2024-09-03 17:31:39 +0545

    cs left

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -3,13 +3,64 @@ import Image from "next/image";
 import Header from "../components/header";
 import Footer from "../footer/footer";
 import Link from "next/link";
-import { topIcon, midIcon, bottomIcon } from "../../assets/aboutImages";
+import {
+  topIcon,
+  midIcon,
+  bottomIcon,
+  leafIcon,
+  bagIcon,
+  boxIcon,
+  carIcon,
+  headphoneIcon,
+  starIcon,
+  checkIcon,
+} from "../../assets/aboutImages";
 import { homeIcon } from "@/assets/signinImages";
+import { buttonIcon } from "../../assets/icons";
 
 import TeamSection from "../team/team";
 import TestimonialDetails from "../testimonial/testimonial";
 
 import Brandlogo from "../brandlogo/brandlogo";
+
+const pageItems = [
+  {
+    id: 1,
+    title: "100% Organic food",
+    text: "100% healthy & Fresh food",
+    icon: leafIcon,
+  },
+  {
+    id: 2,
+    title: "Customer Feedback",
+    text: "Our happy customer",
+    icon: starIcon,
+  },
+  {
+    id: 3,
+    title: "Free Shipping",
+    text: "Free shipping with discount",
+    icon: carIcon,
+  },
+  {
+    id: 4,
+    title: "Great Support 24/7",
+    text: "Instant access to Contact",
+    icon: headphoneIcon,
+  },
+  {
+    id: 5,
+    title: "100% Sucure Payment",
+    text: "We ensure your money is save",
+    icon: bagIcon,
+  },
+  {
+    id: 6,
+    title: "100% Organic Food",
+    text: "100% healthy & Fresh food.",
+    icon: boxIcon,
+  },
+];
 
 const page = () => {
   return (
@@ -42,6 +93,43 @@ const page = () => {
             mi. Nulla eu eros consequat tortor tincidunt feugiat.
           </p>
           <Image src={midIcon} alt="mid" />
+        </div>
+        <div className="grid grid-cols-2">
+          {pageItems.map((about) => (
+            <div key={about.id}>
+              <Image src={about.icon} alt="icons" />
+              <div className="flex">
+                <h3>{about.title}</h3>
+                <p>{about.text}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>
+          <Image src={bottomIcon} alt="bottom" />
+          <h1>We Delivered, You Enjoy Your Order.</h1>
+          <p>
+            Ut suscipit egestas suscipit. Sed posuere pellentesque nunc,
+            ultrices consectetur velit dapibus eu. Mauris sollicitudin dignissim
+            diam, ac mattis eros accumsan rhoncus. Curabitur auctor bibendum
+            nunc eget elementum.
+          </p>
+          <div>
+            <Image src={checkIcon} alt="star" />
+            <p>Sed in metus pellentesque.</p>
+          </div>
+          <div>
+            <Image src={checkIcon} alt="star" />
+            <p>Fusce et ex commodo, aliquam nulla efficitur, tempus lorem.</p>
+          </div>
+          <div>
+            <Image src={checkIcon} alt="star" />
+            <p>Maecenas ut nunc fringilla erat varius.</p>
+          </div>
+          <button className="bg-success text-white px-4 py-3 rounded-full flex items-center ">
+            Shop Now
+            <Image src={buttonIcon} alt="button" className=" ml-4 " />
+          </button>
         </div>
       </section>
       <div className="mt-10">
--- src/assets/aboutImages/index.js
+++ src/assets/aboutImages/index.js
@@ -1,5 +1,23 @@
 import topIcon from "./top.svg";
 import midIcon from "./mid.svg";
 import bottomIcon from "./bottom.svg";
+import leafIcon from "./leaf.svg";
+import bagIcon from "./bag.svg";
+import boxIcon from "./box.svg";
+import carIcon from "./car.svg";
+import headphoneIcon from "./headphone.svg";
+import starIcon from "./star.svg";
+import checkIcon from "./check.svg";
 
-export { topIcon, midIcon, bottomIcon};
+export {
+  topIcon,
+  midIcon,
+  bottomIcon,
+  leafIcon,
+  bagIcon,
+  boxIcon,
+  carIcon,
+  headphoneIcon,
+  starIcon,
+  checkIcon,
+};
